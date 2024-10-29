e7a5eea4a213f541eb3f65e640b601095058510d
Simão Belchior <simao@platforme.com>
2024-10-29 11:39:41 +0000

    Add git file search

--- lua/simaob/remap.lua
+++ lua/simaob/remap.lua
@@ -4,10 +4,11 @@ vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeOpen)
 --- telescope remaps
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find files in git' })
+vim.keymap.set('n', '<leader>fG', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = 'Telescope live grep with arguments' })
+vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-vim.keymap.set('n', '<leader>fG', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = 'Live grep contents of files' })
           
 -- Move Lines
 -- Note: When running on iterm2 for Mac requires changing the leftOption key to send "Meta" in the Profiles
