09e5ef73bd6e5b766febdbb662e20149243271cb
Simão Belchior <simao@platforme.com>
2024-10-29 12:32:45 +0000

    Added remap for fugitive and lazy lock updated.

--- lua/simaob/remaps.lua
+++ lua/simaob/remaps.lua
@@ -1,15 +1,5 @@
---- NvimTree file explorer remaps
-vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeOpen)
-
---- telescope remaps
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find files in git' })
-vim.keymap.set('n', '<leader>fG', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = 'Telescope live grep with arguments' })
-vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-          
+--- GENERIC ---
+---
 -- Move Lines
 -- Note: When running on iterm2 for Mac requires changing the leftOption key to send "Meta" in the Profiles
 -- Settings
@@ -21,4 +11,20 @@ vim.keymap.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=g
 vim.keymap.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
 
+--- PLUGINS ---
 
+--- NvimTree file explorer remaps
+vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeOpen)
+
+--- telescope remaps
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find files in git' })
+vim.keymap.set('n', '<leader>fG', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = 'Telescope live grep with arguments' })
+vim.keymap.set('n', '<leader>fl', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+
+--- Fugitive
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
