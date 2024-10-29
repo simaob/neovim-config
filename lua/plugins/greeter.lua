b5a2f3446d91a1f45a7bcb1f5cd36e5401d533fd
Simão Belchior <simao@platforme.com>
2024-10-29 15:14:07 +0000

    Add greating message

--- lua/plugins/greeter.lua
+++ lua/plugins/greeter.lua
@@ -8,6 +8,14 @@ return {
       -- available: devicons, mini, default is mini
       -- if provider not loaded and enabled is true, it will try to use another provider
       startify.file_icons.provider = "devicons"
+      startify.section.header.val = {
+        "                           _                                                             ",
+        " _____ _               _  | |       _____             _          _____         ___ _     ",
+        "|   __|_|_____ ___ ___| |_|_|___   |   | |___ ___ _ _|_|_____   |     |___ ___|  _|_|___ ",
+        "|__   | |     | .'| . | . | |_ -|  | | | | -_| . | | | |     |  |   --| . |   |  _| | . |",
+        "|_____|_|_|_|_|__,|___|___| |___|  |_|___|___|___|\\_/|_|_|_|_|  |_____|___|_|_|_| |_|_  |",
+        "                                                                                    |___|"
+      }
       require("alpha").setup(
         startify.config
       )
